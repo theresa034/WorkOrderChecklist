2c1edd583ac4a34a4b9f4eca1d87ac878056d056
theresa034 <11844061+theresa034@users.noreply.github.com>
2018-07-13 15:37:58 -0400

    Create top.php

---
+++ top.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>PM Toolkit</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Theresa Glabach">
+        <meta name="description" content="This is a platform for the Project Manager tools">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--[if lt IE 9]>
+        <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+        <![endif]-->
+        
+        //css
+        
+        </head>
