67622188b5a0f9e5b86eb63863c0f5cf61ec49dd
theresa034 <11844061+theresa034@users.noreply.github.com>
2018-07-19 14:21:48 -0400

    Update displayScript.js

--- displayScript.js
+++ displayScript.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 /*hide "extras" from the start*/
-	$(".postageTab").hide();
-	$(".dataProcessing").hide();
-	$(".lettershopFulfillment").hide();
-	$(".print").hide();
-	$(".foreign").hide();
-	$(".warehouse").hide();	
+	$("#postageTab").hide();
+	$("#dataProcessing").hide();
+	$("#lettershopFulfillment").hide();
+	$("#print").hide();
+	$("#foreign").hide();
+	$("#warehouse").hide();	
 	
 /*checkbox conditions*/	
 	$("#chkPostage").click(function(){
 		if($("#chkPostage").is(":checked")){
-			$(".postageTab").show();
+			$("#postageTab").show();
 		} else {
-			$(".postageTab").hide();
+			$("#postageTab").hide();
 		}
 	});
   
