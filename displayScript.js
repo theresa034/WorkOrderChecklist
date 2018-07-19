5d86cfd7a7bb48ba3327b81f18652f203e777ab9
theresa034 <11844061+theresa034@users.noreply.github.com>
2018-07-19 16:08:45 -0400

    Update displayScript.js

--- displayScript.js
+++ displayScript.js
@@ -7,4 +7,52 @@ $(document).ready(function(){
 			$(".postageTab").hide();
 		}
 	});
+	
+	$("#chkDataProcessing").change(function(){
+		if(this.checked){
+			$(".dataProcessing").show();
+		} else {
+			$(".dataProcessing").hide();
+		}
+	});
+	
+	$("#chkLettershop").change(function(){
+		if(this.checked){
+			$(".lettershopFulfillment").show();
+		} else {
+			$(".lettershopFulfillment").hide();
+		}
+	});
+	
+	$("#chkPrint").change(function(){
+		if(this.checked){
+			$(".print").show();
+		} else {
+			$(".print").hide();
+		}
+	});
+	
+	$("#chkForeign").change(function(){
+		if(this.checked){
+			$(".foreign").show();
+		} else {
+			$(".foreign").hide();
+		}
+	});
+
+	$("#chkWarehouse").change(function(){
+		if(this.checked){
+			$(".warehouse").show();
+		} else {
+			$(".warehouse").hide();
+		}
+	});
+	
+	$("#chkSample").change(function(){
+		if(this.checked){
+			$(".sample").show();
+		} else {
+			$(".sample").hide();
+		}
+	});
 });
